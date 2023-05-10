d1d8a1f9d97224dbd4e4e548796aa0084820866d
Batbaatar-Anu <104774983+Batbaatar-Anu@users.noreply.github.com>
2023-05-10 12:15:42 +0800

    Add files via upload

---
+++ loginformm/checkadmin.php
@@ -0,0 +1,9 @@
+<?php
+include 'config.php';
+
+$pmission = $_SESSION['permission'];
+
+if ($pmission!= "admin"){
+    header ('Location:main.php');
+}
+?>
---
+++ loginformm/checkautherize.php
@@ -0,0 +1,10 @@
+<?php
+include "config.php";
+if (!isset($_SESSION['uname'])){
+     header('Location: index.php');
+
+}
+
+
+
+?>
